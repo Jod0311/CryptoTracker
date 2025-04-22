d65f1e43ae6209b3e7f00328c74abc1efa8799b6
Jod0311 <achyuthak2004@gmail.com>
2025-04-22 09:44:49 +0530

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 from ml_models.ml_model import train_model
-
+#test
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
